a357c22770afe5faf7b43f128a7cbade7f334ffb
JunRyung Ju <junryoungju@gmail.com>
2019-06-02 18:59:30 +0900

    Undef if EXIT_FAILURE or EXIT_SUCCESS is defined.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "pch.h"
 #include "driver/CommandLine.h"
 
+#ifdef EXIT_FAILURE
+#undef EXIT_FAILURE
+#endif
 #define EXIT_FAILURE 0xFFFFFFFF
+
+#ifdef EXIT_SUCCESS
+#undef EXIT_SUCCESS
+#endif
 #define EXIT_SUCCESS 0x00000001
 
 namespace GTFW
