b9ce9c793064709225aa6e252c331fa7a6fd4aeb
JunRyung Ju <junryoungju@gmail.com>
2019-06-05 00:34:59 +0900

    Implement of basic tokenlizer structures.

---
+++ src/parser/Parser.cpp
@@ -0,0 +1,86 @@
+#include "parser/Parser.h"
+
+namespace GTFW
+{
+    namespace Parser
+    {
+        Token::Token(TokenType Type, size_t Size, size_t Line, size_t Column)
+        {
+            m_tkType   = Type;
+            m_tkSize   = Size;
+            m_tkLine   = Line;
+            m_tkColumn = Column;
+        }
+
+        TokenType Token::GetTypeOf() const
+        {
+            return m_tkType;
+        }
+
+        bool Token::IsTypeOf(TokenType type) const
+        {
+            return GetTypeOf() == type;
+        }
+
+        bool Token::IsUnknownType() const
+        {
+            return IsTypeOf(TokenType::TOKEN_UNKNOWN);
+        }
+
+        TokenUnknown* Token::AsUnknown()
+        {
+            if (IsUnknownType())
+            {
+                return (TokenUnknown*)this;
+            }
+
+            return nullptr;
+        }
+
+        bool Token::IsLiteralType() const
+        {
+            return IsTypeOf(TokenType::TOKEN_LITERAL);
+        }
+
+        TokenLiteral* Token::AsLiteral()
+        {
+            if (IsLiteralType())
+            {
+                return (TokenLiteral*)this;
+            }
+
+            return nullptr;
+        }
+
+        bool Token::IsSymbolType() const
+        {
+            return IsTypeOf(TokenType::TOKEN_SYMBOL);
+        }
+
+        TokenSymbol* Token::AsSymbol()
+        {
+            if (IsSymbolType())
+            {
+                return (TokenSymbol*)this;
+            }
+
+            return nullptr;
+        }
+
+        bool Token::IsIdentifierType() const
+        {
+            return IsTypeOf(TokenType::TOKEN_IDENTIFIER);
+        }
+
+        TokenIdentifier* Token::AsIdentifier()
+        {
+            if (IsIdentifierType())
+            {
+                return (TokenIdentifier*)this;
+            }
+
+            return nullptr;
+        }
+
+    }
+}
--- src/parser/Parser.h
+++ src/parser/Parser.h
@@ -5,6 +5,89 @@ namespace GTFW
 {
     namespace Parser
     {
+        class TokenUnknown;
+        class TokenLiteral;
+        class TokenSymbol;
+        class TokenIdentifier;
+
+        enum class TokenType
+        {
+            TOKEN_UNKNOWN       = 0xFF,
+            TOKEN_LITERAL       = 0x01,
+            TOKEN_SYMBOL        = 0x02,
+            TOKEN_IDENTIFIER    = 0x03,
+        };
+
+        // class Token
+        // Information :
+        //      Base class of all token objects
+        //      contains basic information of token object, such as size, line, colum, type.
+        class Token
+        {
+            TokenType m_tkType;
+            size_t    m_tkSize;
+            size_t    m_tkLine;
+            size_t    m_tkColumn;
+
+        protected:
+            Token() = delete;
+            Token(TokenType Type, size_t Size, size_t Line, size_t Column);
+
+        public:
+            TokenType GetTypeOf() const;
+            bool IsTypeOf(TokenType type) const;
+
+            bool IsUnknownType() const;
+            TokenUnknown* AsUnknown();
+
+            bool IsLiteralType() const;
+            TokenLiteral* AsLiteral();
+
+            bool IsSymbolType() const;
+            TokenSymbol* AsSymbol();
+
+            bool IsIdentifierType() const;
+            TokenIdentifier* AsIdentifier();
+        };
+
+        class TokenUnknown : Token
+        {
+
+        };
+
+        class TokenLiteral : Token
+        {
+            
+        public:
+            bool IsStringLiteral();
+            bool IsFloatingLiteral();
+            bool IsIntegerLiteral();
+        };
+
+        class TokenSymbol : Token
+        {
+
+        public:
+            bool IsSymbolParen(bool isClosed);
+            bool IsSymbolSquare(bool isClosed);
+            bool IsSymbolSemicolon();
+            bool IsSymbolAsterisk();
+            bool isSymbolCurly(bool isClosed);
+        };
+
+        class TokenIdentifier : Token
+        {
+
+        };
+
+        // class TokenContext
+        // Information:
+        //      This contains token informations for parsed tokens
+        class TokenContext
+        {
+
+        };
+
 
     }
 }
