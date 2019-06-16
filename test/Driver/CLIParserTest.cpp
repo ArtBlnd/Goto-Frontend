53dc6493ebefc08467dd13ac8d56370b6aa5d0dc
JunRyung Ju <junryoungju@gmail.com>
2019-06-16 16:10:06 +0900

    Added new testcases for option parser.

--- test/Driver/CLIParserTest.cpp
+++ test/Driver/CLIParserTest.cpp
@@ -11,20 +11,13 @@ TEST_CASE("CLIParserTestArgs1", "DriverTest")
     const char* argv[] = {
         "gtfw-exec.exe",
         "dummy_source.cpp",
-        "-key1=val",
-        "-key2=val",
+        "-O2",
     };
 
-    bool isCreated = GTFW::Driver::InitCommandLineContext(&CLIContext, 4, argv, nullptr);
+    bool isCreated = GTFW::Driver::InitCommandLineContext(&CLIContext, 3, argv, nullptr);
     REQUIRE(isCreated);
 
-    GTFW::Driver::KVInfo* KV1 = CLIContext->m_clcArgs[0];
-    REQUIRE(KV1->m_Key == "key1");
-    REQUIRE(KV1->m_Val[0] == "val");
-
-    GTFW::Driver::KVInfo* KV2 = CLIContext->m_clcArgs[1];
-    REQUIRE(KV2->m_Key == "key2");
-    REQUIRE(KV2->m_Val[0] == "val");
+    REQUIRE(std::any_cast<bool>(CLIContext->doOptLevel2.GetValOf()));
     
     GTFW::Driver::FreeCommandLineContext(CLIContext);
 }
@@ -34,103 +27,15 @@ TEST_CASE("CLIParserTestArgs2", "DriverTest")
     GTFW::Driver::CommandLineContext* CLIContext = nullptr;
 
     const char* argv[] = {
-        "program",
+        "gtfw-exec.exe",
         "dummy_source.cpp",
-        "-key1=val1,val2",
-        "-key2=val1,val2",
+        "-output-path=\"OutputPath\"",
     };
 
-    bool isCreated = GTFW::Driver::InitCommandLineContext(&CLIContext, 4, argv, nullptr);
+    bool isCreated = GTFW::Driver::InitCommandLineContext(&CLIContext, 3, argv, nullptr);
     REQUIRE(isCreated);
 
-    GTFW::Driver::KVInfo* KV1 = CLIContext->m_clcArgs[0];
-    REQUIRE(KV1->m_Key == "key1");
-    REQUIRE(KV1->m_Val[0] == "val1");
-    REQUIRE(KV1->m_Val[1] == "val2");
-
-    GTFW::Driver::KVInfo* KV2 = CLIContext->m_clcArgs[1];
-    REQUIRE(KV2->m_Key == "key2");
-    REQUIRE(KV2->m_Val[0] == "val1");
-    REQUIRE(KV2->m_Val[1] == "val2");
-
-    GTFW::Driver::FreeCommandLineContext(CLIContext);
-}
-
-TEST_CASE("CLIParserTestArgs3", "DriverTest")
-{
-    GTFW::Driver::CommandLineContext* CLIContext = nullptr;
-
-    const char* argv[] = {
-        "program",
-        "dummy_source.cpp",
-        "-key1=val1,val2,\"val3\"",
-        "-key2=val1,\"val2\",val3",
-    };
-
-    bool isCreated = GTFW::Driver::InitCommandLineContext(&CLIContext, 4, argv, nullptr);
-    REQUIRE(isCreated);
-
-    GTFW::Driver::KVInfo* KV1 = CLIContext->m_clcArgs[0];
-    REQUIRE(KV1->m_Key == "key1");
-    REQUIRE(KV1->m_Val[0] == "val1");
-    REQUIRE(KV1->m_Val[1] == "val2");
-    REQUIRE(KV1->m_Val[2] == "val3");
-
-    GTFW::Driver::KVInfo* KV2 = CLIContext->m_clcArgs[1];
-    REQUIRE(KV2->m_Key == "key2");
-    REQUIRE(KV2->m_Val[0] == "val1");
-    REQUIRE(KV2->m_Val[1] == "val2");
-    REQUIRE(KV2->m_Val[2] == "val3");
-
-    GTFW::Driver::FreeCommandLineContext(CLIContext);
-}
-
-TEST_CASE("CLIParserTestArgs4", "DriverTest")
-{
-    GTFW::Driver::CommandLineContext* CLIContext = nullptr;
-
-    const char* argv[] = {
-        "program",
-        "dummy_source.cpp",
-        "-key1",
-        "-key2",
-    };
-
-    bool isCreated = GTFW::Driver::InitCommandLineContext(&CLIContext, 4, argv, nullptr);
-    REQUIRE(isCreated);
-
-    GTFW::Driver::KVInfo* KV1 = CLIContext->m_clcArgs[0];
-    REQUIRE(KV1->m_Key == "key1");
-    REQUIRE(KV1->m_Val.empty());
-
-    GTFW::Driver::KVInfo* KV2 = CLIContext->m_clcArgs[1];
-    REQUIRE(KV2->m_Key == "key2");
-    REQUIRE(KV2->m_Val.empty());
-
-    GTFW::Driver::FreeCommandLineContext(CLIContext);
-}
-
-TEST_CASE("CLIParserTestArgs5", "DriverTest")
-{
-    GTFW::Driver::CommandLineContext* CLIContext = nullptr;
-
-    const char* argv[] = {
-        "program",
-        "dummy_source.cpp",
-        "-key-1",
-        "-key-2",
-    };
-
-    bool isCreated = GTFW::Driver::InitCommandLineContext(&CLIContext, 4, argv, nullptr);
-    REQUIRE(isCreated);
-
-    GTFW::Driver::KVInfo* KV1 = CLIContext->m_clcArgs[0];
-    REQUIRE(KV1->m_Key == "key-1");
-    REQUIRE(KV1->m_Val.empty());
-
-    GTFW::Driver::KVInfo* KV2 = CLIContext->m_clcArgs[1];
-    REQUIRE(KV2->m_Key == "key-2");
-    REQUIRE(KV2->m_Val.empty());
+    REQUIRE(std::any_cast<std::string>(CLIContext->doOutputPath.GetValOf()) == "OutputPath");
 
     GTFW::Driver::FreeCommandLineContext(CLIContext);
 }
