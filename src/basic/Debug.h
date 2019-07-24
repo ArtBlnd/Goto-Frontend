0d5075287479972847215284daf45508cf52cb74
JunRyung Ju <junryoungju@gmail.com>
2019-07-24 18:37:56 +0900

    Added assertions.

--- src/basic/Debug.h
+++ src/basic/Debug.h
@@ -1,6 +1,18 @@
 #ifndef __GOTO_C_FRONTNED_DEBUG_H__
 #define __GOTO_C_FRONTNED_DEBUG_H__
 
+#define noway_assert(cond, message)
+#define debug_assert(cond, message)
+#define assert(cond, message)
+
+namespace Goto
+{
+    namespace Debug
+    {
+        void Assert(char* reason);
+        void Assert();
+    }
+}
 
 
 #endif
