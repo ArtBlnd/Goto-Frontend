aa08c8e2287ac5da97d6d7fd6a11339adc823b9f
JunRyung Ju <junryoungju@gmail.com>
2019-08-03 18:16:01 +0900

    Added source trace data structures.

---
+++ src/basic/SourceTrace.h
@@ -0,0 +1,38 @@
+#ifndef __GOTO_FRONTEND_BASIC_ERROR_TRACE_H__
+#define __GOTO_FRONTEND_BASIC_ERROR_TRACE_H__
+
+#include <string>
+
+namespace Goto
+{
+    namespace Basic
+    {
+        class ISourceTraceData
+        {
+            
+        };
+
+        class ISourceErrorData : ISourceTraceData
+        {
+
+        };
+
+        class ISourceInfoData : ISourceTraceData
+        {
+
+        };
+
+        class ISourceWarningData : ISourceTraceData
+        {
+
+        };
+
+        class SourceTrace
+        {
+
+        };
+    }
+}
+
+
+#endif
