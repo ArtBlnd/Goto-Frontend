c12df3cecc0ee423007accd577549fa890b15455
JunRyung Ju <junryoungju@gmail.com>
2019-09-11 22:01:35 +0900

    Implemented assertions

--- src/basic/Debug.h
+++ src/basic/Debug.h
@@ -1,9 +1,20 @@
 #ifndef __GOTO_C_FRONTNED_DEBUG_H__
 #define __GOTO_C_FRONTNED_DEBUG_H__
 
-#define noway_assert(cond, message)
+
+#ifdef DEBUG_ASSERT
+#define noway_assert(cond, message) assert(cond, message)
+#define debug_assert(cond, message) assert(cond, message)
+#define assert(cond, message)                                                                                          \
+    if (!cond)                                                                                                         \
+        Goto::Debug::Assert(message)
+#else
+#define noway_assert(cond, message) assert(cond, message)
 #define debug_assert(cond, message)
-#define assert(cond, message)
+#define assert(cond, message)                                                                                          \
+    if (!cond)                                                                                                         \
+        Goto::Debug::Assert(message)
+#endif
 
 namespace Goto
 {
